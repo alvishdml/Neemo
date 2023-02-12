a2b581c430ff8c7e7556310ed2234e8d3bcbb6ac
Alvish <alvish.alvish@digimantra.com>
2023-02-12 16:26:14 +0530

    videoeditor ios

---
+++ ios/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  vesdkreactnativecliintegrationsample
+//
+
+import Foundation
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  /*
+   true - use custom audio browser implementation in this sample.
+   false - use default default implementation.
+   */
+  static let configEnableCustomAudioBrowser = false
+  
+  // Set your Mubert Api key here
+  static let mubertApiKey = ""
+  
+  // Specify name of your project module
+  private let moduleName = "Neemo"
+  private let rootPath = "index"
+  
+  var window: UIWindow?
+  
+  var bridge: RCTBridge!
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    let jsCodeLocation: URL = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: rootPath)
+    
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: moduleName, initialProperties: nil, launchOptions: launchOptions)
+    let rootViewController = UIViewController()
+    
+    rootViewController.view = rootView
+    
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.bridge = rootView.bridge
+    self.window?.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+    return true
+  }
+}
